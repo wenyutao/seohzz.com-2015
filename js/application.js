3f9b30798fdf539829096d8dd7978045e37dd308
zackwong <hzzzoo@126.com>
2015-02-26 04:11:32 +0800

    up

--- js/application.js
+++ js/application.js
@@ -1,6 +1,6 @@
 //= require jquery-1.10.2.min.js
 //= require jquery-migrate-1.2.1.min.js
 //= require bootstrap.js
+//= require jquery.fancybox.pack.js
 //= require back-to-top.js
 //= require app.js
-//= require jquery.fancybox.pack.js
